03c69b4b3b0f4fa5a18580f6484bc06b26dd3152
Maxence Grand <maxencegrand1995@gmail.com>
2018-08-15 23:36:04 +0200

    rovus part

--- report/parts/res/dat_file/ari/script_plot.py
+++ report/parts/res/dat_file/ari/script_plot.py
@@ -10,22 +10,22 @@ if __name__ == "__main__":
     line = datafile.readline()
     colors = ['b','g','r','c','m','y','w']
     iColor=0
-    lex_simple = []
-    lex_simple_add = []
-    lex_lex = []
+    sim_lp = []
+    sim_sp = []
+    mask_lp = []
     nb_kw = []
     dkm = []
-    lex_lex_add = []
+    mask_sp = []
     ae = []
     while line != "":
         print(line)
         tmp = []
         tmp=line.split(" ")
         nb_kw.append(int(tmp[0]))
-        lex_simple.append(float(tmp[3]))
-        lex_simple_add.append(float(tmp[4]))
-        lex_lex.append(float(tmp[1]))
-        lex_lex_add.append(float(tmp[2]))
+        sim_lp.append(float(tmp[3]))
+        sim_sp.append(float(tmp[4]))
+        mask_lp.append(float(tmp[1]))
+        mask_sp.append(float(tmp[2]))
         dkm.append(float(tmp[5]))
         ae.append(float(tmp[6]))
         line = datafile.readline()
@@ -34,12 +34,12 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     fig.set_size_inches(4.5, 3.5)
     #ax.figure(num=None, figsize=(3.5, 1.5), dpi=10, facecolor='w', edgecolor='k')
-    lines += ax.plot(nb_kw, lex_simple,colors[0], label='Simple pretrain', linewidth=2)
-    lines += ax.plot(nb_kw, lex_simple_add,colors[1], label='Simple pretrain, Additional Class', linewidth=2)
-    lines += ax.plot(nb_kw, lex_lex,colors[2], label='Lexical pretrain', linewidth=2)
-    lines += ax.plot(nb_kw, lex_lex_add,colors[3], label='Lexical pretrain, Additional Class', linewidth=2)
-    lines += ax.plot(nb_kw, ae,colors[5], label='Autoencoder + K-Means', linewidth=2)
-    lines += ax.plot(nb_kw, dkm,colors[4], label='Deep K-Means', linestyle=':', linewidth=1)
+    lines += ax.plot(nb_kw, sim_lp,colors[0], label='SIM LP', linewidth=2)
+    lines += ax.plot(nb_kw, sim_sp,colors[1], label='SIM SP', linewidth=2)
+    lines += ax.plot(nb_kw, mask_lp,colors[2], label='MASK SP', linewidth=2)
+    lines += ax.plot(nb_kw, mask_sp,colors[3], label='MASK SP', linewidth=2)
+    #lines += ax.plot(nb_kw, ae,colors[5], label='Autoencoder + K-Means', linewidth=2)
+    #lines += ax.plot(nb_kw, dkm,colors[4], label='Deep K-Means', linestyle=':', linewidth=1)
     
     x = []
     for i in range(len(nb_kw)):
@@ -50,12 +50,12 @@ if __name__ == "__main__":
     fig.gca().xaxis.set_ticklabels(x)
     #fig.legend(loc='upper left', frameon=False)
     
-    fig.legend(lines[:2], ['Simple pretrain', 'Simple pretrain, Additional Class'],
+    fig.legend(lines[:2], ['SIM LP', 'SIM SP'],
                         loc='upper left', frameon=False, prop={'size': 5.5})
-    fig.legend(lines[2:4], ['Lexical pretrain', 'Lexical pretrain, Additional Class'],
+    fig.legend(lines[2:4], ['MASK SP', 'MASK SP'],
                         loc='upper center', frameon=False, prop={'size': 5.5})
-    fig.legend(lines[4:], ['Autoencoder + K-Means', 'Deep K-Means'],
-                        loc='upper right', frameon=False, prop={'size': 5.5})
+    #fig.legend(lines[4:], ['Autoencoder + K-Means', 'Deep K-Means'],
+    #                    loc='upper right', frameon=False, prop={'size': 5.5})
     plt.xlabel('Number of keywords per classes')
     plt.ylabel('ARI')
     fig.savefig(sys.argv[2], bbox_inches='tight')
